8ad8533b6f819caf46530f92c930c3635ba0b84e
SMKozuch <kozuch.samuel@gmail.com>
2018-10-25 13:05:11 +0100

    KBC logging standard

--- main.py
+++ main.py
@@ -30,11 +30,11 @@ sys.tracebacklimit = 3
 
 ### Logging
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format='%(asctime)s - %(levelname)-8s : [line:%(lineno)3s] %(message)s',
     datefmt="%Y-%m-%d %H:%M:%S")
 
-"""
+
 logger = logging.getLogger()
 logging_gelf_handler = logging_gelf.handlers.GELFTCPSocketHandler(
     host=os.getenv('KBC_LOGGER_ADDR'),
@@ -45,7 +45,7 @@ logger.addHandler(logging_gelf_handler)
 
 # removes the initial stdout logging
 logger.removeHandler(logger.handlers[0])
-"""
+
 
 ### Access the supplied rules
 cfg = docker.Config('/data/')
